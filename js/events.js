0c9edcc4b3f1fee3a9e76a9f3830fbcd9a172b0c
Aviad Sapir <aviadsapir@gmail.com>
2018-10-07 16:10:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,21 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('document').on('load',function(){
+    $('img').addclass('tasty');
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
