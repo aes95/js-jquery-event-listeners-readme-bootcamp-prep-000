e34b8d40866a5f2ddfc61911125733556f5f94c7
Aviad Sapir <aviadsapir@gmail.com>
2018-10-07 16:36:58 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,8 +21,8 @@ function pressIt(){
 
 function submitIt(){
   $('form').on('submit', function(){
-    alert()
-  })
+    alert("Your form is going to be submitted now.");
+  });
 }
 
 $(document).ready(function(){
